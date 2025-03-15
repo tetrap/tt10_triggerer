92edca9c229b316fc70b0e856013de9c276a47d2
Krzysztof Skrzynecki <krzysztof@skrzynecki.pl>
2025-03-15 20:58:17 +0100

    add counter and hope the freq to break

--- src/project.sv
+++ src/project.sv
@@ -1,9 +1,26 @@
 /*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2025 Krzysztof Skrzynecki
+ * SPDX-License-Identifier: Beerware
  */
 
 `default_nettype none
+
+module counter#(
+    parameter CNT_WIDTH = 24
+)(
+    input wire clk,
+    input wire rst_n,
+
+    output reg [CNT_WIDTH-1:0] cnt
+);
+  always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+      cnt <= 0;
+    end else begin
+      cnt <= cnt+1;
+    end
+  end
+endmodule
 
 module tt_um_tetrap_triggerer (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -15,13 +32,24 @@ module tt_um_tetrap_triggerer (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  localparam CNT_WIDTH = 24;
+
+  reg [CNT_WIDTH-1:0] master_cnt;
+
+  counter#(.CNT_WIDTH(CNT_WIDTH))cnt0(
+      .clk(clk),
+      .rst_n(rst_n),
+
+      .cnt(master_cnt)
+  );
+
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = ui_in + uio_in + master_cnt[17:10];  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
